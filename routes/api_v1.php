1347854b2c9ee5fad4789c376f3c99b72923ed78
Hasan Hafiz <hasanhafizsakil@gmail.com>
2024-11-06 15:09:22 +0600

    update

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UrlController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\PostController;
-
-// Route::get('/hasan', function(){
-//    return 'API'; 
-// });
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,6 +10,3 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 
 Route::post('/generate-short-url', [UrlController::class, 'createShortUrl'])->middleware('auth:sanctum');
 Route::get('/urls', [UrlController::class, 'getUrls'])->middleware('auth:sanctum');
-
-// Route::apiResource('/posts', PostController::class); 
-// Route::apiResource('/categories', CategoryController::class); 
--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V2\PostController;
+use App\Http\Controllers\UrlController;
+use App\Http\Controllers\Api\V1\AuthController;
 
-// Route::apiResource('posts', PostController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/generate-short-url', [UrlController::class, 'createShortUrl'])->middleware('auth:sanctum');
+Route::get('/urls', [UrlController::class, 'getUrls'])->middleware('auth:sanctum');
